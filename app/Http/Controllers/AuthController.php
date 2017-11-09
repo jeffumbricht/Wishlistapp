bc6f2e890af5d2fb4cf237f56b675a5847323804
Jeff <jeffrey.u@gmail.com>
2017-11-08 21:27:41 -0600

    Added omment about filtering out current user in family dropdown

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -18,6 +18,7 @@ class AuthController extends Controller
 
     $users = User::all();
     // TODO replace with https://laravel.com/docs/5.1/views#view-composers
+    // can't filter out current user because Auth is middleware
     View::share('users', $users);
   }
 
