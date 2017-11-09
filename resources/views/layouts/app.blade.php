dc0841f99da0f0e44ebc0e2da70a0829c1647bd8
Jeff <jeffrey.u@gmail.com>
2017-11-08 22:50:17 -0600

    switched app name from Christmas to Holiday Wishlist to enable future Chanukah and Kwanzaa support

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
   <!-- CSRF Token -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
-  <title>{{ config('app.name', 'Christmas') }}</title>
+  <title>{{ config('app.name', 'Holiday Wishlist') }}</title>
 
   <!-- Styles -->
   <link href="{{ asset('css/main.css') }}" rel="stylesheet">
@@ -29,7 +29,7 @@
 
           <!-- Branding Image -->
           <a class="navbar-brand" href="{{ url('/') }}">
-            {{ config('app.name', 'Christmas') }}
+            {{ config('app.name', 'Holiday Wishlist') }}
           </a>
         </div>
 
